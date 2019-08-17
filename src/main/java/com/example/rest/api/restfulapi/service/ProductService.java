59097d781b01b4c3e4b88f80e92f7fa68d76c1fa
Omar Belahcen <omarbelahcen01@gmail.com>
2019-08-17 18:48:24 +0100

    add pagination feature for products

--- src/main/java/com/example/rest/api/restfulapi/service/ProductService.java
+++ src/main/java/com/example/rest/api/restfulapi/service/ProductService.java
@@ -2,6 +2,10 @@ package com.example.rest.api.restfulapi.service;
 
 import com.example.rest.api.restfulapi.entity.Product;
 import com.example.rest.api.restfulapi.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +21,11 @@ public class ProductService {
 
     public List<Product> retrieveAllProducts(){
         return productRepository.findAll();
+    }
+
+    public Page<Product> retrievePageableProducts(int page, int size){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+        return productRepository.findAll(pageable);
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO product(id,name,price) values (1, 'macbook', 15000)
+INSERT INTO product(id,name,price) values (1, 'macbookPro', 18000)
 INSERT INTO product(id,name,price) values (2, 'imac', 20000)
